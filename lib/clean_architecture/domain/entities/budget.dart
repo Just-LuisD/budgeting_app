2c5c6f8deb259a66193e677333472873f4d4fe19
Just-LuisD <luis.e.duran.engr@gmail.com>
2024-06-21 10:42:57 -0500

    added copy function to the BUdget class

--- lib/clean_architecture/domain/entities/budget.dart
+++ lib/clean_architecture/domain/entities/budget.dart
@@ -1,9 +1,9 @@
 class Budget {
-  final int id;
+  final int? id;
   final String name;
   final double income;
 
-  Budget({required this.id, required this.name, required this.income});
+  Budget({this.id, required this.name, required this.income});
 
   Map<String, dynamic> toMap() {
     return {
@@ -18,6 +18,18 @@ class Budget {
       id: map['id'],
       name: map['name'],
       income: map['income'],
+    );
+  }
+
+  Budget copy({
+    int? id,
+    String? name,
+    double? income,
+  }) {
+    return Budget(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      income: income ?? this.income,
     );
   }
 }
